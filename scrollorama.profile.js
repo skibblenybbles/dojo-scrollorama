629ecd0052da27ee6974cd7a4e11c7f8df569f09
Mike Kibbel <mkibbel@gmail.com>
2012-12-11 11:07:53 -0500

    fixes to the profile.js

--- scrollorama.profile.js
+++ scrollorama.profile.js
@@ -1,27 +1,29 @@
-var 
-    // files to exlude from the mini build
-    miniExcludes = {
-		"README.md": 1,
-		"LICENSE": 1,
-		"package.json": 1,
-		"scrollorama.profile.js": 1
-	},
+var profile = (function() {
+    
+    var
+        // files to exlude from the mini build
+        miniExcludes = {
+    		"README.md": 1,
+    		"LICENSE": 1,
+    		"package.json": 1,
+    		"scrollorama.profile.js": 1
+    	},
 
-	// regular expression for test modules
-	testRx = /(^|\/)tests\//,
+    	// regular expression for test modules
+    	testRx = /(^|\/)tests\//,
     
-	// regular expression for ignoring dojo and util
-	// module ids (for author's development setup)
-	ignoreRx = /^scrollorama\/(dojo|util)\//,
+    	// regular expression for ignoring dojo and util
+    	// module ids (for author's development setup)
+    	ignoreRx = /^scrollorama\/(dojo|util)\//,
     
-	// regular expression for JavaScript filenames
-	jsRx = /\.js$/,
+    	// regular expression for JavaScript filenames
+    	jsRx = /\.js$/;
     
-	// the build profile
-    profile = {
-    
+    // the build profile
+    return {
+
 	    resourceTags: {
-	        
+
 	        ignore: function(filename, mid) {
 	            return ignoreRx.test(mid);
 	        },
@@ -29,13 +31,15 @@ var
     		test: function(filename, mid){
     			return testRx.test(filename);
     		},
-            
+
     		miniExclude: function(filename, mid){
     			return testRx.test(mid) || mid in miniExcludes;
     		},
-            
+
     		amd: function(filename, mid){
     		    return jsRx.test(filename);
     		}
     	}
     };
+    
+})();
